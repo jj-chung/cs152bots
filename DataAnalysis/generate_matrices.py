1a4fe5a20cccd6ea3a112cf9f0e7228b1489ad6d
jj-chung <jenniechung6@gmail.com>
2023-05-31 21:16:53 -0700

    Added updated code

--- DataAnalysis/generate_matrices.py
+++ DataAnalysis/generate_matrices.py
@@ -1,27 +1,25 @@
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 from sklearn import metrics
 
-actual = numpy.random.binomial(1,.9,size = 1000)
-predicted = numpy.random.binomial(1,.9,size = 1000)
-
-confusion_matrix = metrics.confusion_matrix(actual, predicted)
-
-cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [False, True])
-
-cm_display.plot()
-plt.show()
-
-def generate_confusion_matrix():
+def generate_confusion_matrix(actual, predicted, save_name):
     '''
     This function will generate a confusion matrix for the specified dataset
     and save it in the same directory.
+    Arguments:
+        actual - List of actual labels (0s and 1s)
+        predicted - List of predicted 
     '''
+    confusion_matrix = metrics.confusion_matrix(actual, predicted)
+    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["Toxic", "Non-toxic"])
 
+    cm_display.plot()
+    plt.show()
+    plt.savefig(save_name)
 
 if __name__ == '__main__':
     datasets = []
 
     # For each dataset, generate a confusion matrix and save the image
     for dataset in datasets:
-        generate_confusion_matrix()
+        generate_confusion_matrix(actual, predicted, save_name)
